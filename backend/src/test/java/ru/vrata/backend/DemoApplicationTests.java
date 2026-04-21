6a7b4ac819b6c64886bc4b938fcd1c2bcb2bfdb7
Rolan <muliukinrolan@mail.ru>
2026-04-21 20:45:45 +0300

    chore(config): add message encryption key configuration

--- backend/src/test/java/ru/vrata/backend/DemoApplicationTests.java
+++ backend/src/test/java/ru/vrata/backend/DemoApplicationTests.java
@@ -3,7 +3,9 @@ package ru.vrata.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"app.message.encryption.key=MDEyMzQ1Njc4OWFiY2RlZg=="
+})
 class DemoApplicationTests {
 
 	@Test
