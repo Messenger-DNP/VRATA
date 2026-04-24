5e72da3e687294dbadbd9297956656fc9d735d28
Rolan <128055764+pon4ik7@users.noreply.github.com>
2026-04-24 23:03:16 +0300

    fix: consumer problem (#39)

--- backend/src/main/java/ru/vrata/backend/BackendApplication.java
+++ backend/src/main/java/ru/vrata/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package ru.vrata.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
--- backend/src/main/java/ru/vrata/backend/infrastructure/kafka/KafkaConfig.java
+++ backend/src/main/java/ru/vrata/backend/infrastructure/kafka/KafkaConfig.java
@@ -1,13 +1,19 @@
 package ru.vrata.backend.infrastructure.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -27,5 +33,26 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, KafkaMessage> kafkaTemplate(ProducerFactory<String, KafkaMessage> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ConsumerFactory<String, KafkaMessage> consumerFactory(@Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "ru.vrata.backend.infrastructure.kafka");
+        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, KafkaMessage.class);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, KafkaMessage> kafkaListenerContainerFactory(
+            ConsumerFactory<String, KafkaMessage> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, KafkaMessage> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
     }
 }
