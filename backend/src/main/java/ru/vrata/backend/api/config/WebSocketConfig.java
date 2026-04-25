31c4c0cbe9a601943039b71863b1b8dafc4bcf42
Rolan <muliukinrolan@mail.ru>
2026-04-25 10:20:18 +0300

    Add native stomp endpoint

--- backend/src/main/java/ru/vrata/backend/api/config/WebSocketConfig.java
+++ backend/src/main/java/ru/vrata/backend/api/config/WebSocketConfig.java
@@ -32,6 +32,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOriginPatterns(allowedOriginPatterns);
+
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns(allowedOriginPatterns)
                 .withSockJS();
